0cc32dd17380455b2eb407db629b14729f5e20bb
Alaa Almutawa <alalaa@sas.upenn.edu>
2017-06-19 14:02:15 +0300

    basic javascript functions for the memory game

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ var seconds = 0;
 var minutes = 0;
 var hours = 0;
 var t;
+ var gameArray = [];
+
 
 function add() {
     seconds++;
@@ -29,8 +31,66 @@ function removeLevelsDiv(){
     $('.difficulties').addClass("hidden");
 }
 function fillColors(){ //fill the colors behind the disks to be revealed once the disk is clicked
+    var cols = 5;
+    var row = 4;
     var colors = ['#6b4052', '#edcbd4', '#f8cd62', '#7c6258', '#9a986f', '#896539', '#009cb7', '#3f6bae', '#6a5994', '#8fc9c2' ]; //the number of colors have to be hlf the number of the disks
+    colors = shuffleArray(colors);
+    var i = 0;
+    for(var x=0; x<row; x++){
+        gameArray[x] = [];
+        for(var y=0; y<cols; y++){
+            if(i==10){
+                i = 0;
+            }
+            gameArray[x][y]=colors[i];
+            i++;
+        }
+    }
+    gameArray.forEach(shuffleArray);
+    i = 0;
+    var disks = $('div.disk');
+    for(x=0; x<row;x++){
+        for(y=0; y<cols; y++){
+            disks.eq(i).attr("id",''+x+','+y+'');
+            i++;
+        }
+    }
+}
+function showColorFor5Seconds(){
+    var element = $(this);
+    var id = element.attr('id');
+    console.log(id);
+    var fields = id.split(',');
+    var color = gameArray[fields[0]][fields[1]];
+    console.log(color);
+    if($('.disk-clicked').length<2) {
+        element.css('background-color', color);
+        setTimeout(function () {
+            element.css('background-color', '#b9babb');
+            element.removeClass("disk-clicked");
 
+        }, 5000);
+        element.addClass("disk-clicked");
+    }else{
+        $('.disk-clicked').css('background-color','#b9babb');
+        $('.disk-clicked').removeClass("disk-clicked");
+        element.css('background-color', color);
+        setTimeout(function () {
+            element.css('background-color', '#b9babb');
+            element.removeClass("disk-clicked");
+
+        }, 5000);
+        element.addClass("disk-clicked");
+    }
+}
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
 }
 $('#start-btn').on('click', function(){
     //set a timer once clicked and show the user the time spent playing while playing
@@ -40,7 +100,7 @@ $('#start-btn').on('click', function(){
     removeLevelsDiv();
     timer();
     fillColors(); //method that would set each disk to a certian color that can be flipped to be reaveled
-
+    $('.disk').click(showColorFor5Seconds);
 });
 $('#level-btn-easy').on('click',function(){
     $(".difficulties a").removeClass("clicked");
